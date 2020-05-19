bb7af5cee90ac0295d22f108de2bdfc18838b6b9
Karen Lei <karenwinnielei@gmail.com>
2020-05-19 14:10:17 -0700

    complete MVP

--- team-builder/src/components/App.jsx
+++ team-builder/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { v4 as uuid } from 'uuid'
 import '../../src/index.css';
+import Team from './Team'
+import TeamForm from './TeamForm'
 
 const initialTeamRoster = [
   {
@@ -17,18 +19,46 @@ const initialFormValues = {
   role: '', 
 }
 
-function App() {
+export default function App() {
   const [team, setTeam] = useState(initialTeamRoster)
 
   const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = evt => {
+    const { name } = evt.target
+    const { value } = evt.target
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    if( !formValues.name.trim()||
+        !formValues.email.trim()||
+        !formValues.role.trim()
+    ){
+      return
+    }
+
+    const newMember = {...formValues, id: uuid()}
+    setTeam([newMember,...team])
+    setFormValues(initialFormValues)
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Team</h1>
       </header>
+      <TeamForm values ={formValues} onInputChange = {onInputChange} onSubmit = {onSubmit} />
+
+      {
+        team.map(teamMember => {
+          return (
+            <Team key={teamMember.id} details ={teamMember} />
+          )
+        })
+      }
     </div>
   );
 }
 
-export default App;
--- team-builder/src/components/TeamForm.jsx
+++ team-builder/src/components/TeamForm.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 
 export default function TeamForm(props){
-    const {values, onInputChange, onSubmit} = props
+    const {
+        values, 
+        onInputChange, 
+        onSubmit
+    } = props
 
-return
-    <form>
-        
+return (
+    <form onSubmit={onSubmit}>
+        <div>
+            <h2>Add a Team Member</h2>
+            <button>Submit</button>
+        </div>
+
+        <div>
+            <h4>General Information</h4>
+            <label>Name:&nbsp;
+                <input
+                    type = 'text'
+                    placeholder = 'Type a name'
+                    maxLength = '20'
+                    name = 'name'
+                    value = {values.name}
+                    onChange = {onInputChange}
+                />
+            </label>
+            <label>Email:&nbsp;
+                <input
+                    type ='text'
+                    placeholder = 'Type an email'
+                    maxLength = '30'
+                    name = 'email'
+                    value = {values.email}
+                    onChange = {onInputChange}
+                />
+            </label>
+            <label>Role:&nbsp;
+                <select name='role' value={values.role} onChange = {onInputChange}>
+                    <option value =''>Select a Role</option>
+                    <option value = 'Backend Engineer'>Backend Engineer</option>
+                    <option value ='Frontend Engineer'>Frontend Engineer</option>
+                    <option value ='User Experience'>User Experience</option>
+                    <option value ='iOS Developer'>iOS Developer</option>
+                </select>
+            </label>    
+        </div>
     </form>
+)
 }
 
